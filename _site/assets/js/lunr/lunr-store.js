d0b695fb29564bff6cde4295f9a63f56f54d6c62
AMiner <aminer@AMiner.local>
2024-08-11 15:21:38 +0800

    add _site

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,91 @@
+var store = [{
+        "title": "Lhasa Apso",
+        "excerpt":"The Lhasa Apso (/ˈlɑːsə ˈæpsoʊ/ lah-sə ap-soh) is a non-sporting dog breed originating in Tibet. It was bred as an interior sentinel in the Buddhist monasteries, to alert the monks to any intruders who entered. Lhasa is the capital city of Tibet, and apso is a word in the Tibetan...","categories": [],
+        "tags": [],
+        "url": "/pets/lhasa-apso/",
+        "teaser": "/assets/images/500x300.png"
+      },{
+        "title": "Tabby",
+        "excerpt":"A tabby is any domestic cat that has a coat featuring distinctive stripes, dots, lines or swirling patterns, usually together with a mark resembling an ‘M’ on its forehead. Tabbies are sometimes erroneously assumed to be a cat breed. In fact, the tabby pattern is found in many breeds, as...","categories": [],
+        "tags": [],
+        "url": "/pets/tabby/",
+        "teaser": "/assets/images/500x300.png"
+      },{
+        "title": "Baz Boom Identity",
+        "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": [],
+        "tags": [],
+        "url": "/portfolio/baz-boom-identity/",
+        "teaser": "/assets/images/unsplash-gallery-image-1-th.jpg"
+      },{
+        "title": "Fizz Bang Identity",
+        "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": [],
+        "tags": [],
+        "url": "/portfolio/fizz-bang-identity/",
+        "teaser": "/assets/images/unsplash-gallery-image-2-th.jpg"
+      },{
+        "title": "Foo Bar Identity",
+        "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": [],
+        "tags": [],
+        "url": "/portfolio/foo-bar-website/",
+        "teaser": "/assets/images/foo-bar-identity-th.jpg"
+      },{
+        "title": "Ginger Gulp Identity",
+        "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": [],
+        "tags": [],
+        "url": "/portfolio/ginger-gulp-identity/",
+        "teaser": "/assets/images/unsplash-gallery-image-3-th.jpg"
+      },{
+        "title": "Quick-select k-th smallest elements in lists",
+        "excerpt":"快速选择算法基于两种partition算法实现，在两种不同的partition算法会产生完全不同的算法实现， 深入了解不同的划分方式差异，对算法理解和实现有重要作用。 两类划分算法 Lomuto partition scheme Hoare partition scheme 与 Hoare partition 不同的是 Lomutopartition算法返回pivot的下标k， partition算法将小于等于pivot的元素放第k个元素左边，大于pivot的元素放在第k个位置右边， 因此第k小的元素就是pivot自己。 Lomuto Partition Scheme: // implementation of &lt;Introduction of algorithm&gt; (从小到大排序) int partition(vector&lt;int&gt;&amp; a, int l, int r) { int x = a[r], i = l - 1; for (int j = l; j &lt;...","categories": ["Algorithm"],
+        "tags": ["Divide-and-Conquer","Quick Sort Algorithm"],
+        "url": "/algorithm/quick-select/",
+        "teaser": "/assets/images/500x300.png"
+      },{
+        "title": "Chocolate Chip Cookies",
+        "excerpt":"A chocolate chip cookie is a drop cookie that originated in the United States and features chocolate chips as its distinguishing ingredient. The traditional recipe combines a dough composed of butter and both brown and white sugar with semi-sweet chocolate chips. Variations include recipes with other types of chocolate as...","categories": [],
+        "tags": [],
+        "url": "/recipes/chocolate-chip-cookies/",
+        "teaser": "/assets/images/500x300.png"
+      },{
+        "title": "Oatmeal Cookies",
+        "excerpt":"Oatmeal cookies are a proverbial favorite with both kids and adults. This crisp and chewy cookie is loaded with oats, dried fruit, and chopped nuts. Ingredients 1 cup butter, softened 1 cup white sugar 1 cup packed brown sugar 2 eggs 1 teaspoon vanilla extract 2 cups all-purpose flour 1...","categories": [],
+        "tags": [],
+        "url": "/recipes/oatmeal-cookies/",
+        "teaser": "/assets/images/500x300.png"
+      },{
+        "title": "Peanut Butter Cookies",
+        "excerpt":"A peanut butter cookie is a type of cookie that is distinguished for having peanut butter as a principal ingredient. The cookie generally originated in the United States, its development dating back to the 1910s. Ingredients 1 cup unsalted butter 1 cup crunchy peanut butter 1 cup white sugar 1...","categories": [],
+        "tags": [],
+        "url": "/recipes/peanut-butter-cookies/",
+        "teaser": "/assets/images/500x300.png"
+      },{
+    "title": "Page Not Found",
+    "excerpt":"Sorry, but the page you were trying to view does not exist.  ","url": "http://localhost:4000/404.html"
+  },{
+    "title": "About",
+    "excerpt":"TODO  ","url": "http://localhost:4000/about/"
+  },{
+    "title": "categories",
+    "excerpt":" ","url": "http://localhost:4000/categories/"
+  },{
+    "title": "Collections",
+    "excerpt":"pets Lhasa Apso The Lhasa Apso is a non-sporting dog breed originating in Tibet. Tabby A tabby is any domestic cat that has a coat featuring distinctive stripes, dots, lines or swirling patterns, usually with a mark resembling an ‘M’ on its for... portfolio Baz Boom Identity Baz Boom design...","url": "http://localhost:4000/collection-archive/"
+  },{
+    "title": "Tags",
+    "excerpt":"","url": "http://localhost:4000/tags/"
+  },{
+    "title": "Terms and Privacy Policy",
+    "excerpt":"## Privacy Policy The privacy of my visitors is extremely important. This Privacy Policy outlines the types of personal information that is received and collected and how it is used. First and foremost, I will never share your email address or any other personal information to anyone without your direct...","url": "http://localhost:4000/terms/"
+  },{
+    "title": "Posts",
+    "excerpt":"","url": "http://localhost:4000/year-archive/"
+  },{
+    "title": "Divide-and-Conquer",
+    "excerpt":"","url": "http://localhost:4000/tags/divide-and-conquer/"
+  },{
+    "title": "Quick Sort Algorithm",
+    "excerpt":"","url": "http://localhost:4000/tags/quick-sort-algorithm/"
+  },{
+    "title": "Algorithm",
+    "excerpt":"","url": "http://localhost:4000/categories/algorithm/"
+  }]
